2dcfcf73e1e929f89533ca0393e7cbcf78b13822
hammad786 <hammad786@users.noreply.github.com>
2016-11-23 23:33:33 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -2,5 +2,3 @@ Tic Tac Toe
 ========
 
 This is a simple Javascript implementation of the game Tic Tac Toe using the minimax algorithm for the computer player. The human player will at best tie the game.
-
-Demo link: http://www.hammadbaig.com/ttt/
--- assets/ui.js
+++ assets/ui.js
@@ -2,8 +2,8 @@ var bdiv = document.createElement("div");
 var att = document.createAttribute("id");
 att.value = "board";
 bdiv.setAttributeNode(att);
-document.getElementById("main").appendChild(bdiv);
+document.getElementById("container").appendChild(bdiv);
 for (var i = 0; i < 9; i++)
   document.getElementById("board").innerHTML += '<div class="cell" onclick="human_turn(this)"></div>';
-document.getElementById("board").innerHTML +='<div id="reset" class="button" onclick="reset()">Reset Game</div>';
-document.getElementById("board").innerHTML +='<div id="letai" class="button" onclick="reset(); aiturn(state, 0, cPlayer, true);">Let AI Start!</div>';
+document.getElementById("board").innerHTML +='<div id="restart" class="button" onclick="restart()">Restart Game</div>';
+document.getElementById("board").innerHTML +='<div id="letai" class="button" onclick="restart(); restart(); ai(state, current_player, true);">Let AI Start!</div>';
